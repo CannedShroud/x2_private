24b5895f23baf6c129337d6242ec6692c24c68ed
CannedShroud <hypersync112@gmail.com>
2021-10-18 02:45:11 +0530

    X2: device: Build 2.1 biometric hal as well

--- device.mk
+++ device.mk
@@ -89,7 +89,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.x2 \
-    vendor.oppo.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1 \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1 
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
